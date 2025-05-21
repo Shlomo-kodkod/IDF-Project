b4d774445e35bbe549d46582eebf692592130289
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-21 17:23:41 +0300

    add class Aman

---
+++ Aman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    public class Aman
+    {
+        private Terrorist terroristDada;
+        private string lastLocation;
+        private DateTime timeStemp;
+
+        public Aman(Terrorist terroristDada, string leastLocation, DateTime timeStemp)
+        {
+            this.terroristDada = terroristDada;
+            this.lastLocation = leastLocation;
+            this.timeStemp = timeStemp;
+        }   
+
+        public void UpdateLocation(DateTime timeStemp)
+        {
+            this.timeStemp = timeStemp;
+        }
+    }
+}
