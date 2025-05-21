69b3b3e5ea22b1edc40855217ec051008266f412
Shlomo <shlomot575@gmail.com>
2025-05-21 14:58:49 +0300

    add class Terrorist

---
+++ Terrorist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    public class Terrorist
+    {
+        private string Name;
+        private int Rank;
+        private bool Status;
+        private string[] Weapons;   
+
+        public Terrorist(string Name, int Rank, bool Status, string[] Weapons)
+        {
+            this.Name = Name;
+            this.Rank = Rank;
+            this.Status = Status;
+            this.Weapons = Weapons;
+        }
+
+
+        public int GetRank()
+        {
+            return Rank;
+        }
+
+        public bool GetStatus()
+        {
+            return Status;
+        }
+
+        public string[] GetWeapons()
+        {
+            return Weapons;
+        }
+        public void ChangeStatus()
+        {
+            Status = !Status;
+        }
+
+        
+    }
+
+
+}
