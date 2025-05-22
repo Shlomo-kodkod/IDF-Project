6fd589aef52842b66473775d7fc93aaf972b24f0
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-22 11:38:06 +0300

    change class name

---
+++ ReportAman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    public class ReportAman
+    {
+        private Terrorist terroristDada;
+        private string lastLocation;
+        private DateTime timeStemp;
+
+        public ReportAman(Terrorist terroristDada, string leastLocation, DateTime timeStemp)
+        {
+            this.terroristDada = terroristDada;
+            this.lastLocation = leastLocation;
+            this.timeStemp = timeStemp;
+        }   
+
+        public void UpdateLocation(string lastLocation, DateTime timeStemp)
+        {
+            this.lastLocation = lastLocation;
+            this.timeStemp = timeStemp;
+        }
+    }
+}
