7e2dd3799c08ceafd49282ee6d8efcc1267691eb
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-22 16:12:50 +0300

    add DataAnalyzer class

---
+++ DataAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    public class DataAnalyzer
+    {
+        public static Terrorist FindTerroristByReport(List<ReportAman> report)
+        {
+            return report[0].GetTerrorist();
+        }
+        public static Terrorist MostReportedTerorist(Dictionary<string, List<ReportAman>> terrorists)
+        {
+            int maxReported = 0;
+            List<ReportAman> currentList = new List<ReportAman>();
+            int currentReportNum = 0;
+
+            foreach (KeyValuePair<string, List<ReportAman>> item in terrorists)
+            {
+                currentReportNum = item.Value.Count();
+
+                if (currentReportNum > maxReported)
+                {
+                    maxReported = currentReportNum;
+                    currentList = item.Value;
+                }
+            }
+            return FindTerroristByReport(currentList);
+        }
+    }
+}
