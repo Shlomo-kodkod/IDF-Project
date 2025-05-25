4f3c36229c7a5e63f98ddbf3533ebc586a81a5b5
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-25 20:39:03 +0300

    edit strike execution class

--- StrikeExecution.cs
+++ StrikeExecution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,35 @@ namespace IDF_Project
 {
     public class StrikeExecution
     {
+        private Dictionary<string, string> attackMap;
+        public StrikeExecution()
+        {
+            this.attackMap = new Dictionary<string, string>
+            {
+                { "building", "F16 Fighter Jet" },
+                { "people",  "Zik drone" },
+                { "vehicle", "Zik drone" },
+                { "open area","M109 Artillery"}
+            };
+        }
         public static DateTime GetCurrentTime()
         {
             DateTime currentTime = DateTime.Now;
             return currentTime;
         }
 
-        public static StrikeOptions ChoseAttackWeapon(Terrorist terrorist)
+        public string ChoseAttackWeapon(Terrorist terrorist, string location)
         {
-
+            return this.attackMap[location];
         }
 
 
         public static bool IsValidName(string name)
         {
+            if (name == "")
+            {
+                return false;
+            }
             foreach(char ch in name)
             {
                 if (! char.IsLetter(ch))
@@ -33,9 +49,15 @@ namespace IDF_Project
         }
         public static string GetOfficerName()
         {
-            Console.WriteLine("Enter your name: ");
-            string Name = Console.ReadLine();
-            return Name;
+            string name = "";
+            do
+            {
+                Console.WriteLine("Enter your name: ");
+                name = Console.ReadLine();
+                return name;
+            }
+            while (!IsValidName(name));
+            
         }
     }
 }
