c2f57c89005d8dce99529f39fa777626d0779986
mosheshulman <msh36345@gmail.com>
2025-05-27 18:23:02 +0300

    strike log

--- StrikeLog.cs
+++ StrikeLog.cs
@@ -7,16 +7,35 @@ public class StrikeLog
     private StrikeOptions TypeStrike;
     private string OperatorName;
     private ReportAman LastReport;
-    private int LeftAmmo;
+    public static List<StrikeLog> logs = new List<StrikeLog>();
 
-    StrikeLog(Terrorist terrorist , StrikeOptions typeStrike,string operatorName, ReportAman lastReport, int leftAmmo)
+    public StrikeLog(Terrorist terrorist, StrikeOptions typeStrike, string operatorName, ReportAman lastReport, int leftAmmo)
     {
         CurentTime = DateTime.Now;
         Terrorist = terrorist;
         TypeStrike = typeStrike;
         OperatorName = operatorName;
         LastReport = lastReport;
-        LeftAmmo = typeStrike.GetCapacity();
+
+        logs.Add(this);
+    }
+    
+    void PrintLastLog()
+    {
+        Console.WriteLine(logs[logs.Count-1]);
     }
 
+    void PrintLog(int logNum)
+    {
+        Console.WriteLine(logs[logNum-1]);
+    }
+
+    public override string ToString()
+    {
+        return $"Time of Strike : {CurentTime}\n" +
+               $"Terrorist : {Terrorist.ToString()}\n" +
+               $"Last report : {LastReport.GetLastLocation()} at {LastReport.GetLastTimeStemp()}\n" +
+               $"Strike weapon : {TypeStrike.ToString()}\n" +
+               $"Operator name : {OperatorName}\n";
+    }
 }
